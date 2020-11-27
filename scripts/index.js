1a3d99447cc659474474a7bd00ccb342a8926584
talha7t <48598366+talha7t@users.noreply.github.com>
2020-11-28 01:31:38 +0500

    added about page

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,20 @@
-let hamMenu = document.querySelector('.navbar__toggle');
+let hamMenu = document.querySelector('.navbar-toggle');
 let nav = document.getElementById("navigation");
+let isNavOpen = false;
 
-
-gsap.to('.community-participant-count', {innerText: 180, duration: 4} )
+gsap.from('.community-participant-count, .campus-ambassadors-count, .counter-collaborations-count, .counter-initiative-count',
+ {innerText: 0, duration: 3} )
 
 
 hamMenu.addEventListener('click', ()=> {
-  nav.classList.toggle("navigation-visible");
-})
+  // nav.classList.toggle("navigation-visible");
+  if(!isNavOpen) {
+    gsap.to('.navigation', {duration: 1, height: 'auto'})
+    isNavOpen = true;
+  } else {
+    gsap.to('.navigation', {duration: 1, height: '0' });
+    isNavOpen = false;
+  }
+  
+}, {passive: true})
 
