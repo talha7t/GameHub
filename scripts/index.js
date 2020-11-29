983fc944a7083ac9673708db85d0acc8bfd328fb
talha7t <48598366+talha7t@users.noreply.github.com>
2020-11-29 11:51:10 +0500

    working with gsap

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,10 @@ let hamMenu = document.querySelector('.navbar-toggle');
 let nav = document.getElementById("navigation");
 let isNavOpen = false;
 
-gsap.from('.community-participant-count, .campus-ambassadors-count, .counter-collaborations-count, .counter-initiative-count',
- {innerText: 0, duration: 3} )
+const communityPaticipant = document.querySelector('.community-participant-count')
+
+// gsap.from('.community-participant-count, .campus-ambassadors-count, .counter-collaborations-count, .counter-initiative-count',
+//  {onUpdate: ()=>{communityPaticipant.innerHTML = }, duration: 3} )
 
 
 hamMenu.addEventListener('click', ()=> {
@@ -18,3 +20,16 @@ hamMenu.addEventListener('click', ()=> {
   
 }, {passive: true})
 
+window.addEventListener('scroll', ()=> {
+  let scroll = window.scrollY;
+  if(scroll > 10) {
+    gsap.set('.header', {transformOrigin: '50% bottom'});
+
+    gsap.to('.header', {height: 0, duration:0.5, ease: 'power2.out'});
+    //  use from to here
+    gsap.to('.navbar', {position: 'fixed', width: '100%', duration: 0.5, top: '0', zIndex: '2'});
+  } else {
+    gsap.to('.header', {height: 'auto', duration: 0.5, ease: 'power2.out'});
+    gsap.to('.navbar', {position: 'static'})
+  }
+})
